da5341e670ffbd93ddc5c6f066cb8d60e94ead24
bas132828 <63089699+bas132828@users.noreply.github.com>
2020-12-28 14:49:23 +0300

    Update index.js

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -2,7 +2,13 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for ( var i = 0; i < numberOfButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-  var audio = new Audio('sounds/tom-1.mp3');
-  audio.play();
+    this.style.color = "red";
   });
 }
+
+
+
+
+//
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
