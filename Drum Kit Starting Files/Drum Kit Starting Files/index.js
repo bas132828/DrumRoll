c833b277c520c89c6e8ad4e6476d95aaf46db40c
bas132828 <63089699+bas132828@users.noreply.github.com>
2020-12-28 12:00:28 +0300

    Update index.js

---
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,6 @@
+document.querySelectorAll("button")[0].addEventListener("click", function () {
+  alert("I am clicked");
+});
+document.querySelectorAll("button")[1].addEventListener("click", function () {
+  alert("I am clicked");
+});
